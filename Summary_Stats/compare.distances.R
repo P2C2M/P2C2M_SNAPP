2fee2dd1542bbca8d432deb098ce2d9db1f457f4
P2C2M <49522632+P2C2M@users.noreply.github.com>
2019-06-01 13:13:06 -0400

    Delete compare.distances.R

--- Summary_Stats/compare.distances.R
+++
@@ -1,9 +0,0 @@
-### Function for comparing tree distances to assess significance ###
-
-compare.distances <- function(dists, distrib){ # dists = distances between posterior tree and random simulated posterior trees; distrib = distribution of distances between randomly drawn simulated posterior trees
-  
-  quant <- quantile(distrib, probs = c(0.95)) # get value separating upper 5% of distribution (use one tail because small distance values are good)
-  bad <- sum(length(which(dists > quant))) # sum the number of ditances outside of 95% distribution
-  
-  return(bad)
-}
