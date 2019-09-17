67cdc65ef0488d60aca0c608aeeaa20072516d3f
P2C2M <49522632+P2C2M@users.noreply.github.com>
2019-09-17 11:36:36 -0400

    Delete P2C2M.SNAPP.R

--- R/P2C2M.SNAPP.R
+++
@@ -1,9 +0,0 @@
-#' \code{P2C2M.SNAPP} An R package for conducting posterior predictive checks of SNAPP analyses.
-#'
-#' P2C2M.SNAPP contains a single function that uses SNAPP output to simulate posterior predictive datasets and uses those datasets to identify violations #' to the Multispecies Coalescent Model implemented in SNAPP.
-#'
-#' A tutorial with example files is included with the package, as well as an example script for snalyzing posterior predictive datasets with SNAPP.
-#'
-#' @docType package
-#' @name P2C2M.SNAPP
-NULL
